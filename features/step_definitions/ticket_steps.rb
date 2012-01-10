cc3840789c21272999f593d183f395a003d96b21
Ruben Cancho <canchete@gmail.com>
2012-01-10 17:01:40 +0100

    When creating tickets, attribute them to the creator.

--- features/step_definitions/ticket_steps.rb
+++ features/step_definitions/ticket_steps.rb
@@ -3,3 +3,11 @@ Given /^that project has a ticket:$/ do |table|
     @project.tickets.create!(attributes)
   end
 end
+
+Given /^"([^"]*)" has created a ticket for this project:$/ do |email, table|
+  user = User.find_by_email!(email)
+  table.hashes.each do |attributes|
+    attributes = attributes.merge!(:user => user)
+    @project.tickets.create!(attributes)
+  end
+end
